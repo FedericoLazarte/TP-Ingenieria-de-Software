c2f83bed9f5bbe31683357354f0fcc738ef33357
Federico Lazarte <fedelazarte2015@gmail.com>
2025-10-03 21:25:32 -0300

    add asistencia

---
+++ js/asistencia.js
@@ -0,0 +1,19 @@
+const listaAsistencia = [
+  { id: 'asistencia1', nombre: 'Centro Salud Norte', coordenadas: [51.503, -0.08], horario: '08:00 - 18:00' }
+];
+
+
+function mostrarAsistencia() {
+  const contenedor = document.getElementById('asistencia');
+  contenedor.innerHTML = '<h2>Centros de Asistencia</h2>';
+  listaAsistencia.forEach(a => {
+    let tarjeta = document.createElement('div');
+    tarjeta.className = 'tarjeta';
+    tarjeta.innerHTML = `<h3>${a.nombre}</h3><p>Horario: ${a.horario}</p>`;
+    tarjeta.onclick = () => centrarEnMarcador(a.id);
+    contenedor.appendChild(tarjeta);
+    agregarMarcador(a.id, a.coordenadas, a.nombre);
+  });
+}
+
+mostrarAsistencia();
